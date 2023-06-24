39b83f82df6ba04ebc7e4d6cea4afd407f1b162b
Vladimir Denisenko <101186417+AbuAdam07@users.noreply.github.com>
2023-06-24 18:14:42 +0300

    1

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,8 +18,8 @@ module.exports = {
       chainId: 1337
     },
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/SdVItf_NwJ7whx4g88ZXLn0O7ew9eqIW",
-      accounts: [ "dbd5a099102c8f0fdf582568162aea3aec0e2dc8f2bc6e41ff0209be919ae24f" ]
+      url: "ALCHEMY_URL",
+      accounts: [ "PRIVATE-KEY" ]
     }
   },
   solidity: {
