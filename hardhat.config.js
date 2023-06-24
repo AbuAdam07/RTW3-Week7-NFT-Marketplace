7cd9874e086f7e2896d5766278dc76f501c87015
Vladimir Denisenko <101186417+AbuAdam07@users.noreply.github.com>
2023-06-24 18:14:31 +0300

    баг при открытии сминченых нфт

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,12 +18,12 @@ module.exports = {
       chainId: 1337
     },
     goerli: {
-      url: "<YOUR_ALCHEMY_URL>",
-      accounts: [ "<YOUR_PRIVATE_KEY>" ]
+      url: "https://eth-goerli.g.alchemy.com/v2/SdVItf_NwJ7whx4g88ZXLn0O7ew9eqIW",
+      accounts: [ "dbd5a099102c8f0fdf582568162aea3aec0e2dc8f2bc6e41ff0209be919ae24f" ]
     }
   },
   solidity: {
-    version: "0.8.4",
+    version: "0.8.5",
     settings: {
       optimizer: {
         enabled: true,
